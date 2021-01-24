ac71b7c7ac20b150fd83f7a96ad8811ca71fe07c
Matt Whipple <whipplem123@gmail.com>
2021-01-24 00:23:55 -0500

    Retrieve map data dynamically
    - Get map data from API in stadiums-website-backend
    - Remove hard-coded map data
    - Remove logo files

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,7 @@
 import {GoogleMap, Marker} from '@react-google-maps/api';
 import React from 'react';
 
-const oms = require(`npm-overlapping-marker-spiderfier/lib/oms.min`);
-const mlbMarkers = require('./mlb');
-
+const oms = require('npm-overlapping-marker-spiderfier/lib/oms.min');
 let spiderfy;
 
 const onMapLoad = (map) => {
@@ -15,23 +13,35 @@ const onMarkerLoad = (marker) => {
 };
 
 class StadiumsMap extends React.Component {
+    state = {
+        mapMarkers: []
+    }
+
+    componentDidMount() {
+        fetch('https://e4oec3pez7.execute-api.us-west-2.amazonaws.com/prod/stadiums/map')
+            .then(res => res.json())
+            .then(data => this.setState({ mapMarkers: data.mapData }))
+            .catch(err => console.error('Failed to retrieve map data: ' + err));
+    }
+
     render() {
         return <GoogleMap
-            id='data-example'
+            id='stadiums-map'
             mapContainerStyle={{height: '600px'}}
             zoom={4.5}
             center={{lat: 39.8097343, lng: -98.5556199 }}
             onLoad={onMapLoad}
         >
-        {mlbMarkers.map((marker) => {
+        {this.state.mapMarkers.map((marker) => {
             return <Marker
-                name={marker.name}
+                key={marker.teamId}
+                name={marker.teamId}
                 position={marker.position}
                 onLoad={onMarkerLoad}
                 icon={{
-                url: marker.file,
-                anchor: new window.google.maps.Point(...marker.anchor),
-                scaledSize: new window.google.maps.Size(...marker.scaledSize)
+                    url: marker.logoUrl,
+                    anchor: new window.google.maps.Point(marker.markerSize.x / 2, marker.markerSize.y / 2),
+                    scaledSize: new window.google.maps.Size(marker.markerSize.x, marker.markerSize.y)
                 }}
             />;
         })}
