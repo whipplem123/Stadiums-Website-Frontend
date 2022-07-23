ddfaeefeb964f1bfa622f33646f9ce0198b30f89
Matt Whipple <whipplem123@gmail.com>
2022-07-23 17:46:19 -0400

    Add info windows for stadiums (hard-coded data)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React, { Component} from 'react';
 import './App.css';
 import {LoadScript} from '@react-google-maps/api';
 import StadiumsMap from './Map';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const leagueSelectedEvent = (eventKey) => {
+  alert(`Selected league is ${eventKey}`);
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-		<LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
-			<StadiumsMap />
-		</LoadScript>
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
+          <div className="MapsFilterMenu">
+            <DropdownButton id="MapsDropdown" style={{'z-index': 999}} title="League">
+              <Dropdown.Item onSelect={leagueSelectedEvent} eventKey="MLB">MLB</Dropdown.Item>
+              <Dropdown.Item onSelect={leagueSelectedEvent} eventKey="NFL">NFL</Dropdown.Item>
+              <Dropdown.Item onSelect={leagueSelectedEvent} eventKey="NBA">NBA</Dropdown.Item>
+              <Dropdown.Item onSelect={leagueSelectedEvent} eventKey="NHL">NHL</Dropdown.Item>
+              <Dropdown.Item onSelect={leagueSelectedEvent} eventKey="MLS">MLS</Dropdown.Item>
+              <Dropdown.Item onSelect={leagueSelectedEvent} eventKey="NCAAF">NCAAF</Dropdown.Item>
+              <Dropdown.Item onSelect={leagueSelectedEvent} eventKey="NCAAB">NCAAB</Dropdown.Item>
+            </DropdownButton>
+          </div>
+          <div className="StadiumsMap">
+            <StadiumsMap />
+          </div>
+        </LoadScript>
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,41 +1,53 @@
-import {GoogleMap, Marker} from '@react-google-maps/api';
+import {GoogleMap, Marker, InfoBox} from '@react-google-maps/api';
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 
 const oms = require('npm-overlapping-marker-spiderfier/lib/oms.min');
 let spiderfy;
 
-const onMapLoad = (map) => {
-	spiderfy = new oms.OverlappingMarkerSpiderfier(map, {});
-};
-
 const onMarkerLoad = (marker) => {
-	spiderfy.addMarker(marker);
+    spiderfy.addMarker(marker);
 };
 
 class StadiumsMap extends React.Component {
     state = {
-        mapMarkers: []
+        mapMarkers: [],
+        activeMarker: null
     }
+
+    onMapLoad = (map) => {
+        spiderfy = new oms.OverlappingMarkerSpiderfier(map, {keepSpiderfied: true});
+        spiderfy.addListener('click', (marker, event) => {
+            const activeMarker = this.state.activeMarker === marker.title ? null : marker.title;
+            this.setState({activeMarker});
+            console.log(event);
+        });
+    };
 
     componentDidMount() {
         fetch('https://e4oec3pez7.execute-api.us-west-2.amazonaws.com/prod/stadiums/map')
             .then(res => res.json())
-            .then(data => this.setState({ mapMarkers: data.mapData }))
+            .then(data => this.setState({ mapMarkers: data.mapData, activeMarker: null }))
             .catch(err => console.error('Failed to retrieve map data: ' + err));
     }
 
     render() {
+        console.log(this.state.activeMarker);
         return <GoogleMap
             id='stadiums-map'
             mapContainerStyle={{height: '600px'}}
-            zoom={4.5}
+            zoom={4}
             center={{lat: 39.8097343, lng: -98.5556199 }}
-            onLoad={onMapLoad}
+            onLoad={this.onMapLoad}
+            onClick={() => this.setState({activeMarker: null})}
         >
         {this.state.mapMarkers.map((marker) => {
             return <Marker
+                id={marker.teamId}
                 key={marker.teamId}
                 name={marker.teamId}
+                title={marker.teamId}
                 position={marker.position}
                 onLoad={onMarkerLoad}
                 icon={{
@@ -43,7 +55,40 @@ class StadiumsMap extends React.Component {
                     anchor: new window.google.maps.Point(marker.markerSize.x / 2, marker.markerSize.y / 2),
                     scaledSize: new window.google.maps.Size(marker.markerSize.x, marker.markerSize.y)
                 }}
-            />;
+            >
+                {marker.teamId === this.state.activeMarker && <InfoBox
+                    key={`infowindow-${marker.teamId}`}
+                    onCloseClick={() => this.setState({activeMarker: null})}
+                >
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="https://img.mlbstatic.com/mlb-images/image/private/t_2x1/t_w1536/mlb/b4cwfrwczafhg9jmzy4q.jpg" />
+                        <Card.Body>
+                            <Card.Title>Great American Ball Park</Card.Title>
+                            <Table borderless={true}>
+                                <tbody>
+                                    <tr>
+                                        <th>Team</th>
+                                        <td>Cincinnati Reds</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Location</th>
+                                        <td>Cincinnati, OH</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Founded</th>
+                                        <td>1892</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Capacity</th>
+                                        <td>45,000</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                            <Card.Link href="https://google.com">Read More</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </InfoBox>}
+            </Marker>;
         })}
         </GoogleMap>;
     }
